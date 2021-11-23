1982d56ae8313f1a38473f3baff326e404a176c7
Valentina Bearzotti <valebearzotti1@gmail.com>
2021-11-22 22:59:22 -0300

    moving stuff

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Navbar() {
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
+
+const Header = styled.div`
+    display: flex;
+`
+
+export default Navbar
