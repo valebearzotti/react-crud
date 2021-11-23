09987120e93913b6ecade6c4252515a717b963d4
Valentina Bearzotti <valebearzotti1@gmail.com>
2021-11-22 23:10:35 -0300

    styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,14 +3,25 @@ import styled from 'styled-components'
 
 function Navbar() {
     return (
-        <div>
-            Home
-        </div>
+        <Header>
+            <Logo>Home</Logo>
+            <div>Home</div>
+        </Header>
     )
 }
 
 const Header = styled.div`
     display: flex;
+    flex-direction: row;
+    padding: 1em;
+    div:nth-child(2){
+        margin-left: auto;
+    }
+`
+
+const Logo = styled.div`
+    color: #54b0bf;
+    font-weight: bold;
 `
 
 export default Navbar
